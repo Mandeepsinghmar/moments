a1f2266d46dbeb89320a7e81474e4d43deb29a8b
Mandeep Singhmar <msingh8328@gmail.com>
2024-09-24 18:56:40 +0530

    custom upload button

---
+++ src/app/_components/simple-upload-button.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useUploadThing } from "~/utils/uploadthing";
+
+// inferred input off useUploadThing
+type Input = Parameters<typeof useUploadThing>;
+
+const useUploadThingInputProps = (...args: Input) => {
+  const $ut = useUploadThing(...args);
+
+  const onChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
+
+    const selectedFiles = Array.from(e.target.files);
+    const result = await $ut.startUpload(selectedFiles);
+
+    console.log("uploaded files", result);
+    // TODO: persist result in state maybe?
+  };
+
+  return {
+    inputProps: {
+      onChange,
+      multiple: ($ut.routeConfig?.image?.maxFileCount ?? 1) > 1,
+      accept: "image/*",
+    },
+    isUploading: $ut.isUploading,
+  };
+};
+
+function UploadSVG() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      className="size-6"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
+      />
+    </svg>
+  );
+}
+
+function LoadingSVG() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      className="size-6"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+      />
+    </svg>
+  );
+}
+
+export default function SimpleUploadButton() {
+  const router = useRouter();
+  const { inputProps, isUploading } = useUploadThingInputProps(
+    "imageUploader",
+    {
+      onClientUploadComplete() {
+        router.refresh();
+      },
+    },
+  );
+  return (
+    <div>
+      {isUploading ? (
+        <label htmlFor="upload-button">
+          <LoadingSVG />
+        </label>
+      ) : (
+        <div>
+          <label htmlFor="upload-button" className="cursor-pointer">
+            <UploadSVG />
+          </label>
+          <input
+            id="upload-button"
+            type="file"
+            className="sr-only"
+            {...inputProps}
+          />
+        </div>
+      )}
+
+      {isUploading && <div>Uploading...</div>}
+    </div>
+  );
+}
--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,33 +1,23 @@
-"use client";
-
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { useRouter } from "next/navigation";
-import { UploadButton } from "~/utils/uploadthing";
+import SimpleUploadButton from "./simple-upload-button";
 
 export default function TopNav() {
-  const router = useRouter();
   return (
     <div className="flex justify-between border-b p-4">
       <div className="text-4xl font-extrabold text-pink-500">t3gallery</div>
-      <div>
+      <div className="flex items-center gap-4">
         <SignedIn>
-          <div>
-            <h2 className="p-4 text-center text-2xl">Upload New Images</h2>
-            <UploadButton
-              endpoint="imageUploader"
-              onClientUploadComplete={() => router.refresh()}
-            />
-          </div>
+          <SimpleUploadButton />
         </SignedIn>
-      </div>
-      <div className="text-2xl">
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
+        <div className="text-2xl">
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
 
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
       </div>
     </div>
   );
