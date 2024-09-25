8defc520da61c8987b50a0e7e8a5202baf2b1576
Mandeep Singhmar <msingh8328@gmail.com>
2024-09-25 14:45:44 +0530

    added upload event using posthog

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from "next/navigation";
 import { useUploadThing } from "~/utils/uploadthing";
 import { toast } from "sonner";
+import { usePostHog } from "posthog-js/react";
 
 // inferred input off useUploadThing
 type Input = Parameters<typeof useUploadThing>;
@@ -72,10 +73,13 @@ function LoadingSVG() {
 
 export default function SimpleUploadButton() {
   const router = useRouter();
+  const posthog = usePostHog();
+
   const { inputProps, isUploading } = useUploadThingInputProps(
     "imageUploader",
     {
       onUploadBegin() {
+        posthog?.capture("upload-begin");
         toast(
           <div className="flex items-center gap-2 text-lg text-white">
             <LoadingSVG /> Uploading...
